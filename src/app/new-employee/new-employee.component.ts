f4342af17066f5749ca0fa4be226f3434ac506b4
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-04-13 11:37:32 -0400

    feat(core): adds router

--- src/app/new-employee/new-employee.component.ts
+++ src/app/new-employee/new-employee.component.ts
@@ -1,14 +1,28 @@
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
     selector: 'app-new-employee',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     template: `
-        <p>
-            employee-list works!
-        </p>
+        <h2>Create New Employee</h2>
+        <form>
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" required> <br/> <br/>
+            <label for="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" required> <br/> <br/>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required> <br/><br/>
+            <label for="phone">Phone:</label>
+            <input type="tel" id="phone" name="phone" required> <br/><br/>
+            <label for="age">Age:</label>
+            <input type="number" id="age" name="age" required> <br/><br/>
+            <button type="button" routerLink="/">Go back</button>
+            <button type="submit">Create</button>
+
+        </form>
     `,
     styles: []
 })
