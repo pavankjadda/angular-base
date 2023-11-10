88529afb4c8f912fbbe6af61f78b69203a27a569
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-11-10 01:30:54 -0500

    chore: Refactor employee list component and routes

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,33 +1,30 @@
-import {CommonModule} from '@angular/common';
-import {Component, inject, OnInit, signal} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {EmployeeService} from '../services/employee.service';
-import {Employee} from '../types/Employee';
-import {ViewEmployeeComponent} from '../view-employee/view-employee.component';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { EmployeeService } from '../services/employee.service';
+import { Employee } from '../types/Employee';
+import { ViewEmployeeComponent } from '../view-employee/view-employee.component';
 
 @Component({
-    selector: 'app-employee-list',
-    standalone: true,
-    imports: [CommonModule, ViewEmployeeComponent, RouterLink, RouterOutlet],
-    template: `
-        <h2>Employees</h2>
-        <div *ngFor="let employee of employees()">
-            <app-view-employee [employee]="employee"/>
-        </div>
-        <button routerLink="/employee/new">Create New</button>
-    `,
-    styles: []
+	selector: 'app-employee-list',
+	standalone: true,
+	imports: [CommonModule, ViewEmployeeComponent, RouterLink, RouterOutlet],
+	template: `
+		<h2>Employees</h2>
+		<div *ngFor="let employee of employees()">
+			<app-view-employee [employee]="employee" />
+		</div>
+		<button routerLink="/employee/new">Create New</button>
+	`,
+	styles: [],
 })
-export class EmployeeListComponent implements OnInit
-{
-    employeeService = inject(EmployeeService);
-    employees = signal<Employee[]>([]);
+export class EmployeeListComponent implements OnInit {
+	employeeService = inject(EmployeeService);
+	employees = signal<Employee[]>([]);
 
-    ngOnInit(): void
-    {
-        this.employeeService.getEmployees().subscribe((data) =>
-        {
-            this.employees.set(data);
-        });
-    }
+	ngOnInit(): void {
+		this.employeeService.getEmployees().subscribe((data) => {
+			this.employees.set(data);
+		});
+	}
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,19 +1,18 @@
-import {Routes} from '@angular/router';
-import {EmployeeListComponent} from './employee-list/employee-list.component';
-import {NewEmployeeComponent} from './new-employee/new-employee.component';
-import {ViewEmployeeComponent} from './view-employee/view-employee.component';
-
+import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { NewEmployeeComponent } from './new-employee/new-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 export const routes: Routes = [
-    {
-        path: '',
-        component: EmployeeListComponent
-    },
-    {
-        path: 'employee/new',
-        component: NewEmployeeComponent
-    },
-    {
-        path: 'employee/view',
-        component: ViewEmployeeComponent
-    }
+	{
+		path: '',
+		component: EmployeeListComponent,
+	},
+	{
+		path: 'employee/new',
+		component: NewEmployeeComponent,
+	},
+	{
+		path: 'employee/view',
+		component: ViewEmployeeComponent,
+	},
 ];
