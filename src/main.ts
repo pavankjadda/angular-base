ce738ff44bbe2e6d932b341087c0669e92f6dc64
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-05-24 13:36:16 -0400

    fix(imports): fixes HTTP client module

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-	providers: [importProvidersFrom(BrowserModule, RouterModule.forRoot(routes), HttpClientModule)],
+	providers: [importProvidersFrom(BrowserModule, RouterModule.forRoot(routes)), provideHttpClient(withFetch())],
 }).catch((err) => console.error(err));
