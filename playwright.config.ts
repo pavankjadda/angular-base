7b446a0000088dfd20e33055f39759c0fbc62eac
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-07-05 16:54:30 -0400

    feat(core): adds setup.js

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,20 +22,20 @@ const config: PlaywrightTestConfig = {
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+	forbidOnly: !!process.env['CI'],
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env['CI'] ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 4 : 4,
+	workers: process.env['CI'] ? 4 : 4,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'dot' : 'list',
+	reporter: process.env['CI'] ? 'dot' : 'list',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		//baseURL: process.env.CI ? process.env.URL : 'http://localhost:4200',
-		baseURL: process.env.URL,
+		//baseURL: process.env['CI'] ? process.env.URL : 'http://localhost:4200',
+		baseURL: process.env['URL'],
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
 		screenshot: 'off',
