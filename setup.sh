a20382021c31870fb620a528642aa69ee1f20ea7
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-07-05 16:44:51 -0400

    feat(core): adds setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Prompt the user for the project name
+read -r -p "What's the project name in lower case with no spaces? " projectName
+
+# Define the directory to search in and the directories to exclude
+searchDir="."
+excludeDirs=(.angular node_modules)
+
+# Construct the find command excluding specified directories
+findCommand="find $searchDir"
+for dir in "${excludeDirs[@]}"; do
+  findCommand+=" ! -path '*/$dir/*'"
+done
+findCommand+=" -type f"
+
+# Execute the find command and replace occurrences of "angular-base" with the project name
+eval "$findCommand" | while read -r file; do
+  # Check if the file is a text file before replacing
+  if file "$file" | grep -q text; then
+    sed -i '' "s/angular-base/$projectName/g" "$file"
+    echo "Updated $file"
+  fi
+done
+
+# Rename the angular-base directory to the project name
+cd ..
+mv angular-base "$projectName"
+cd "$projectName" || exit
